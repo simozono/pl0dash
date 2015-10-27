eb6fd9bf3d89d2a9d52e8efc8538b4162ae7d37a
Koichi SHIMOZONO <simozono@cc.kagoshima-u.ac.jp>
2015-10-27 16:39:04 +0900

    parse00 に多くのコメントを入れた。あとエラー処理を分離した。

--- parser00.c
+++ parser00.c
@@ -7,19 +7,25 @@ extern FILE *yyin;
 extern int yylex();
 extern char *yytext;
 
-int getToken(void);
+int getToken(void); /* トークンを取得する関数 */
 void parse_Expression(void);
 void parse_Expression_dash(void);
 void parse_Term(void);
 void parse_Term_dash(void);
 void parse_Factor(void);
+void parse_error(char *error_message); /* エラーメッセージを出す */
 
 int nextToken; /* 次のトークンが入る変数 */
 
-int getToken(void) { /* トークンを取得する関数 */
+int getToken(void) {
   int token = yylex();
   if (token == 0) {token = T_EOF; } /* yylex()が0を返す時がEOFのようだ */
   return token;
+}
+
+void parse_error(char *error_message) {
+  printf ("parse error: %s\n", error_message);
+  exit(0);
 }
 
 int main(int argc, char *argv[]) {
@@ -28,20 +34,18 @@ int main(int argc, char *argv[]) {
     exit(0);
   }
 
-  yyin = fopen(argv[1], "r");
+  yyin = fopen(argv[1], "r"); /* ファイルを開く処理 */
   if (yyin  == NULL) {
     printf ("%s file not found.\n", argv[1]);
     exit(0);
   }
 
-  nextToken = getToken();
+  /* 最初に1トークン読み込んでおく */
+  nextToken = getToken(); 
 
   /* 構文解析スタート S=E */
   parse_Expression();
-  if (nextToken != T_EOF) {
-    printf ("parse error EOF\n");
-    exit(0);
-  }
+  if (nextToken != T_EOF) parse_error("not EOF");
 }
 
 void parse_Expression() { /* E → TE' */
@@ -58,12 +62,12 @@ void parse_Expression_dash() { /* E' → +TE'|ε */
   }
 }
 
-void parse_Term() {
+void parse_Term() { /* T → FT' */
   parse_Factor();
   parse_Term_dash();
 }
 
-void parse_Term_dash() {
+void parse_Term_dash() { /* T' → *FT'|ε */
   if (nextToken == T_MULTI) {
     nextToken = getToken();
     parse_Factor();
@@ -72,20 +76,16 @@ void parse_Term_dash() {
   }
 }
 
-void parse_Factor() {
+void parse_Factor() { /* F → (E)|T_NUMBER */
   if (nextToken == T_LPAR) {
     nextToken = getToken();
     parse_Expression();
-    if (nextToken != T_RPAR) {
-      printf ("error right paren\n");
-      exit(0);
-    }
+    if (nextToken != T_RPAR) parse_error("not )");
     nextToken = getToken();
   } else if (nextToken == T_NUMBER) {
     printf ("process number %s\n", yytext);
     nextToken = getToken();
   } else {
-    printf ("error token\n");
-    exit(0);
+    parse_error("not number and not (");
   }
 }
